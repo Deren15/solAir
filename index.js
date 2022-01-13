a876f64f02a183a07d96b5def8424d6d3868e30f
Deren Y <67863795+Deren15@users.noreply.github.com>
2022-01-13 07:29:03 +0000

    wallet balance

---
+++ index.js
@@ -0,0 +1,31 @@
+const {
+ Connection,
+ PublicKey,
+ clusterApiUrl,
+ Keypair,
+ LAMPORTS_PER_SOL,
+ Transaction,
+ Account,
+} = require("@solana/web3.js");
+
+const newPair = new Keypair();
+console.log("newpair => ", newPair)
+const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+
+const secretKey = newPair._keypair.secretKey
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = await Keypair.fromSecretKey(secretKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`=> For wallet address ${publicKey}`);
+        console.log(`   Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
